67cbd83d78258fd10d21860f3f9ec7a32f948dbe
v310r <webmail200601@gmail.com>
2021-07-07 22:51:38 +0300

    added bfs_and_dfs

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 
 #define INF 10000
 
 typedef int weight;
+typedef int vertex;
 
 class Graph{
+
 private:
+
     std::vector<std::vector<weight>> matrix_of_adjacency;
     int vertexes;
     int edges;
+
 public:
+
     void input(){
         std::cout<<"Enter number of vertexes: ";
         std::cin>> vertexes;
@@ -42,6 +49,51 @@ public:
             }
             std::cout<<std::endl;
         }
+        std::cout<<std::endl;
+    }
+
+    void print_bfs(vertex start){
+        std::vector<bool> used(vertexes,false);
+        std::queue<vertex> q;
+        q.push(start);
+        used[start] = true;
+        std::cout<<"bfs traverse: "<<start<<" ";
+        while(!q.empty()) {
+            vertex current = q.front();
+            q.pop();
+            vertex v{};
+            for(auto weight:matrix_of_adjacency[current]) {
+                if((weight < INF) && (!used[v])){
+                    std::cout<<v<<" ";
+                    used[v] = true;
+                    q.push(v);
+                }
+                ++v;
+            }
+        }
+        std::cout<<std::endl;
+    }
+
+        void print_dfs(vertex start){
+        std::vector<bool> used(vertexes,false);
+        std::stack<vertex> s;
+        s.push(start);
+        used[start] = true;
+        std::cout<<"dfs traverse: "<<start<<" ";
+        while(!s.empty()) {
+            vertex current = s.top();
+            s.pop();
+            vertex v{};
+            for(auto weight:matrix_of_adjacency[current]) {
+                if((weight < INF) && (!used[v])){
+                    std::cout<<v<<" ";
+                    used[v] = true;
+                    s.push(v);
+                }
+                ++v;
+            }
+        }
+        std::cout<<std::endl;
     }
 };
 
@@ -49,5 +101,6 @@ int main(){
     Graph g;
     g.input();
     g.print();
-
+    g.print_bfs(1);
+    g.print_dfs(1);
 }
